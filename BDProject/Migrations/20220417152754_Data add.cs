fae0266870905ffd425b1b6f425b38de6601a52c
Ksenya <snegurka64@mail.ru>
2022-04-18 11:46:45 +0400

    Добавьте файлы проекта.

---
+++ BDProject/Migrations/20220417152754_Data add.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDProject.Migrations
+{
+    public partial class Dataadd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Teams_Team1Id",
+                table: "Games");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Teams_Team2Id",
+                table: "Games");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Teams_Team1Id",
+                table: "Games",
+                column: "Team1Id",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Teams_Team2Id",
+                table: "Games",
+                column: "Team2Id",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Teams_Team1Id",
+                table: "Games");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Teams_Team2Id",
+                table: "Games");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Teams_Team1Id",
+                table: "Games",
+                column: "Team1Id",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Teams_Team2Id",
+                table: "Games",
+                column: "Team2Id",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ BDProject/Migrations/20220418065441_Add queries.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BDProject.Migrations
+{
+    public partial class Addqueries : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
